34647bf0c4e33aa377c2d84554de10ac1070cd6b
Vlad172-3 <golovshy@gmail.com>
2023-05-24 14:20:48 +0200

    first commit

---
+++ jQueryApp.js
@@ -0,0 +1,8 @@
+$(".addHtmlt").html("<h1> New Element </h1>");
+
+$(".divHide").hide();
+
+$("section").remove();
+
+$("div:nth-of-type(4) div").addClass("addClass");
+$("div:nth-of-type(4) div").html("<span>Lumos Maxima</span>");
